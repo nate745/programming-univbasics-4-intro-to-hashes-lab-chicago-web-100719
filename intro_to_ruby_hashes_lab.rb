88bff6c2e29afd1ce0f1b527d1f2c427a0eac1ec
Nathan Alex <nate745@gmail.com>
2019-10-05 06:46:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,10 +24,10 @@ end
 
 def update_counting_hash(hash, key)
 
-  if hash[:key]
-    hash[:key] += 1
+  if hash[key]
+    hash[key] += 1
   else
-    hash[:key] = 1
+    hash[key] = 1
   end
 
 end
